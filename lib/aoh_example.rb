fdc9c6d646c928e7ee1549ebfcefb1b8793f6fbc
Alex Foreman <foreman.alexander@gmail.com>
2020-07-07 18:19:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,17 +7,23 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-
-end
-
-def literal_aoh
-    result = [
+   result = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
    ]
+end
+
+def literal_aoh
+ result = [
+  DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+  JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+  KATE_G = { name:  "Kate Gompert", occupation: "None" },
+  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+]
 end
 
 def aoh_lookup(aoh, row, key)
