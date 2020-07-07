2909b9b9e949575050bb78b811388232e64951e2
Alex Foreman <foreman.alexander@gmail.com>
2020-07-07 18:22:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,6 +26,5 @@ aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
-end
+  aoh[row][key] = new_value
+end 
